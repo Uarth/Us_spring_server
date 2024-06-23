af53771d3eba3ab2c72177475dfe1f9e411f7488
allofjw <jw990416@yonsei.ac.kr>
2024-06-23 16:51:57 +0900

    chore : .env update
    .

--- src/main/java/ybigta/us/UsApplication.java
+++ src/main/java/ybigta/us/UsApplication.java
@@ -2,11 +2,17 @@ package ybigta.us;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+
 
 @SpringBootApplication
 public class UsApplication {
 
 	public static void main(String[] args) {
+		// OAuth2.0 비공개정보
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("CLIENT_ID", dotenv.get("CLIENT_ID"));
+		System.setProperty("CLIENT_SECRET", dotenv.get("CLIENT_SECRET"));
 		SpringApplication.run(UsApplication.class, args);
 
 
