35486b278c0cc838879a2a4e1a7eaca3fceca376
Chaerim <cofla2020@yonsei.ac.kr>
2024-06-26 01:23:24 +0900

    feat: connect model server to spring for saving features after inference

--- src/main/java/ybigta/us/dto/FeatureResponse.java
+++ src/main/java/ybigta/us/dto/FeatureResponse.java
@@ -1,6 +1,7 @@
 package ybigta.us.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 import ybigta.us.domain.Question1;
 import ybigta.us.domain.Question2;
 import ybigta.us.domain.Question3;
@@ -10,35 +11,43 @@ import java.util.List;
 @Getter
 @Setter
 public class FeatureResponse {
-    private List<Float> audio_feature;
-    private List<Float> pos;
-    private String trans_text;
-    private List<Float> emb_text;
+    private int status;
+    private String message;
+    private Features features;
+
+    @Getter
+    @Setter
+    public static class Features {
+        private List<Float> audio_feature;
+        private String transcribed_text;
+        private List<Float> pos_feature;
+        private List<Float> text_embedding;
+    }
 
     public Question1 toQuestion1Entity() {
         Question1 question1 = new Question1();
-        question1.setAudioFeature(this.audio_feature);
-        question1.setPos(this.pos);
-        question1.setTransText(this.trans_text);
-        question1.setEmbText(this.emb_text);
+        question1.setAudioFeature(this.features.getAudio_feature());
+        question1.setPos(this.features.getPos_feature());
+        question1.setTransText(this.features.getTranscribed_text());
+        question1.setEmbText(this.features.getText_embedding());
         return question1;
     }
 
     public Question2 toQuestion2Entity() {
         Question2 question2 = new Question2();
-        question2.setAudioFeature(this.audio_feature);
-        question2.setPos(this.pos);
-        question2.setTransText(this.trans_text);
-        question2.setEmbText(this.emb_text);
+        question2.setAudioFeature(this.features.getAudio_feature());
+        question2.setPos(this.features.getPos_feature());
+        question2.setTransText(this.features.getTranscribed_text());
+        question2.setEmbText(this.features.getText_embedding());
         return question2;
     }
 
     public Question3 toQuestion3Entity() {
         Question3 question3 = new Question3();
-        question3.setAudioFeature(this.audio_feature);
-        question3.setPos(this.pos);
-        question3.setTransText(this.trans_text);
-        question3.setEmbText(this.emb_text);
+        question3.setAudioFeature(this.features.getAudio_feature());
+        question3.setPos(this.features.getPos_feature());
+        question3.setTransText(this.features.getTranscribed_text());
+        question3.setEmbText(this.features.getText_embedding());
         return question3;
     }
 }
