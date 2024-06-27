04d9ddc8f6abd632eeb0fdc603b2a693e14538ce
Chaerim <cofla2020@yonsei.ac.kr>
2024-06-28 00:32:23 +0900

    fix: question repo에 함수 추가

--- src/main/java/ybigta/us/repository/Question1Repository.java
+++ src/main/java/ybigta/us/repository/Question1Repository.java
@@ -2,11 +2,12 @@ package ybigta.us.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ybigta.us.domain.Question1;
-import ybigta.us.domain.Question2;
-import ybigta.us.dto.FeatureResponse;
+
+import java.util.Optional;
 
 public interface Question1Repository extends JpaRepository<Question1, Integer> {
     Question1 save(Question1 question1);
-    Question1 updateQuestion(Integer userId, Question1 updateQuestion );
+    Optional<Question1> findById(Integer userId);
+    boolean existsById(Integer userId);
 
 }
