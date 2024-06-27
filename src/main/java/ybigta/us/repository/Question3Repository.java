d3dd66ac5534005b44b628d233f7544860d8bddd
Chaerim <76841211+C-limlim@users.noreply.github.com>
2024-06-28 00:36:59 +0900

    fix: repo 인터페이스에 함수 추가 (#27)
    * chore: add readme and gitignore
    
    * feat: add s3 upload api (#9)
    
    * fix: change gitignore
    
    * feat: connect rds and add test query for user table (#10)
    
    * chore: add example properties
    
    * feat: Update gradle.yml for aws/database properties
    
    * fix: docker permission error
    
    * fix: docker permission error
    
    * fix: change order in gradle.yml
    
    * chore: set aws credentials for remote build
    
    * Add oauth2 login  (#2)
    
    * chore: mirror push (#1)
    
    * chore: add readme and gitignore
    
    * feat: add s3 upload api (#9)
    
    * fix: change gitignore
    
    * feat: connect rds and add test query for user table (#10)
    
    * chore: add example properties
    
    * feat: Update gradle.yml for aws/database properties
    
    * fix: docker permission error
    
    * fix: docker permission error
    
    * fix: change order in gradle.yml
    
    * chore: set aws credentials for remote build
    
    * feat : OAuth2.0 Login
    
    1. 로그인기능 추가
    2. Thymeleaf default에 맞춘 레포짓구조변경
    
    * chore : .env update
    
    .
    
    * chore: .env update
    
    cool sms update
    
    ---------
    
    Co-authored-by: Chaerim <76841211+C-limlim@users.noreply.github.com>
    
    * sms certification (#3)
    
    * chore: mirror push (#1)
    
    * chore: add readme and gitignore
    
    * feat: add s3 upload api (#9)
    
    * fix: change gitignore
    
    * feat: connect rds and add test query for user table (#10)
    
    * chore: add example properties
    
    * feat: Update gradle.yml for aws/database properties
    
    * fix: docker permission error
    
    * fix: docker permission error
    
    * fix: change order in gradle.yml
    
    * chore: set aws credentials for remote build
    
    * feat : OAuth2.0 Login
    
    1. 로그인기능 추가
    2. Thymeleaf default에 맞춘 레포짓구조변경
    
    * feat : sms Certification용 메세지전송 Controller
    
    * feat : User Table 성별, 휴대폰번호 추가
    
    * test : SessionData확인용 Controller
    
    * refactor : 로직에 안맞는 클래스 위치수정
    
    Role, OAuthAttributes가 dto에 들어있어서 상위레포짓으로 옮겼습니다.
    
    ---------
    
    Co-authored-by: Chaerim <76841211+C-limlim@users.noreply.github.com>
    
    * fix : Signup update (#4)
    
    * chore: Update gradle.yml
    
    add env file
    
    * fix: move .env to root (#6)
    
    * FEAT: 모델 인퍼런스 서버와 피처 테이블 연결 (#7)
    
    * fix: 다른 구현 기능 동안 잠시 인가인증 취소
    
    * feat: add insert query to question tables
    
    * feat: connect model server to spring for saving features after inference
    
    * FEAT: addsession,cookie (#11)
    
    * feat : Token validation
    
     feat : user dashboard (미완)
    
    * feat : add featureselect (#13)
    
    * FEAT: add dashboard api (#15)
    
    * feat : add featureselect
    
    * feat : add dashboard api
    
    ---------
    
    Co-authored-by: Chaerim <76841211+C-limlim@users.noreply.github.com>
    
    * Jongwon/dashboard api (#17)
    
    * feat : add featureselect
    
    * feat : add dashboard api
    
    * fix : 프론트에 반환할 value 추가
    
    * Jongwon/signup api (#18)
    
    * feat : add featureselect
    
    * feat : add dashboard api
    
    * fix : 프론트에 반환할 value 추가
    
    * feat : add signup api
    
    ---------
    
    Co-authored-by: Chaerim <76841211+C-limlim@users.noreply.github.com>
    
    * FEAT: 매칭 서버 연결 (#19)
    
    * feat: add connect matching server
    
    * FIX: Modify matchingdto
    
    * fix: typo and session in s3 (#22)
    
    * Chaerim/modify feature reposit (#24)
    
    * fix: typo and session in s3
    
    * fix: save duplicate row in question with same user_id
    
    * fix: repsitory interface에 함수 추가 (#26)
    
    * fix: typo and session in s3
    
    * fix: save duplicate row in question with same user_id
    
    * fix: question repo에 함수 추가
    
    ---------
    
    Co-authored-by: allofjw <155447305+allofjw@users.noreply.github.com>

--- src/main/java/ybigta/us/repository/Question3Repository.java
+++ src/main/java/ybigta/us/repository/Question3Repository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ybigta.us.domain.Question3;
 import ybigta.us.dto.FeatureResponse;
 
+import java.util.Optional;
+
 public interface Question3Repository extends JpaRepository<Question3, Integer> {
     Question3 save(Question3 question3);
+    boolean existsById(Integer userId);
+    Optional<Question3> findById(Integer userId);
 }
